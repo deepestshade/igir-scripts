d8dc218dd5be7936c2e8a8a61985240480ce2c70
Chris <chris@wookie.local>
2025-08-26 20:38:48 -0700

    Create microsd

--- create-microsd.sh
+++ create-microsd.sh
@@ -3,38 +3,45 @@
 input_dir="roms-sorted"
 output_dir="microsd"
 
+igir() {
+  npx --yes igir@latest "$@"
+}
+
 echo "Copying BIOS files..."
-npx igir@latest copy zip test \
+igir copy extract test \
   --input-checksum-quick \
   --dat "https://raw.githubusercontent.com/libretro/libretro-database/master/dat/System.dat" \
-  --input "${input_dir}/" \
+  --input  "${input_dir}/BIOS" \
   --output "${output_dir}/bios"
 
 echo "Copying No-Intro files..."
-npx igir@latest copy zip test \
+igir copy zip test \
   --input-checksum-quick \
   --dat "dat/No-Intro (Reduced Set).zip" \
-  --input "${input_dir}/No-Intro" \
+  --input  "${input_dir}/No-Intro" \
   --output "${output_dir}/{es}"
 
 echo "Copying Redump files..."
-npx igir@latest copy zip test \
+igir copy test \
   --input-checksum-quick \
   --dat "dat/Redump*.zip" \
-  --input "${input_dir}/Redump" \
+  --input  "${input_dir}/Redump" \
   --output "${output_dir}/{es}"
 
 echo "Copying MAME files..."
-npx igir@latest copy zip test \
+igir copy zip test \
   --input-checksum-quick \
-   --dat "dat/MAME 0.279.zip" \
-   --input "${input_dir}/MAME" \
-   --output "${output_dir}/mame"
-find "${output_dir}/mame" -mindepth 2 -type f ! -name "*.*" -exec mv "{}" "{}.chd" \;
+  --dat "dat/MAME 0.279.zip" \
+  --input  "${input_dir}/MAME" \
+  --output "${output_dir}/mame"
+
+# Rename CHD files that lack extensions
+find "${output_dir}/mame" -mindepth 2 -type f ! -name "*.*" \
+  -exec mv "{}" "{}.chd" \;
 
 echo "Copying FBNeo files..."
- npx igir@latest copy zip test \
+igir copy zip test \
   --input-checksum-quick \
   --dat "dat/FBNeo*.zip" \
-  --input "${input_dir}/FBNeo" \
-  --output "${output_dir}/fbneo" 
+  --input  "${input_dir}/FBNeo" \
+  --output "${output_dir}/fbneo"
