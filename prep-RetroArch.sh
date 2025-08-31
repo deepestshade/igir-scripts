63249d34dc5601eb496d4d98126655f11f318b83
Chris <chris@wookie.local>
2025-08-31 07:35:22 -0700

    Support FBNeo 1.0.0.3

---
+++ prep-RetroArch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+input_dir="./intermediate"
+output_dir="./output/RetroArch"
+
+igir() {
+  npx --yes igir@latest \
+    "$@" \
+    --input-checksum-max CRC32 \
+    --input-checksum-archives never
+}
+
+mkdir -p "${output_dir}"
+
+echo "Copying BIOS files..."
+igir copy extract clean test \
+  --dat "https://raw.githubusercontent.com/libretro/libretro-database/master/dat/System.dat" \
+  --input  "${input_dir}/BIOS" \
+  --output "${output_dir}/bios"
+
+echo "Copying No-Intro roms..."
+igir copy zip clean test \
+  --dat "./dat/proper1g1r-collection.zip" \
+  --input  "${input_dir}/No-Intro" \
+  --output "${output_dir}/{es}"
+
+echo "Copying Redump roms..."
+igir copy clean test \
+  --dat "./dat/Redump*.zip" \
+  --input "${input_dir}/Redump" \
+  --output "${output_dir}/{es}" \
+  --disable-cache
+
+echo "Copying FBNeo roms..."
+igir copy zip clean test \
+  --dat "./dat/FBNeo 1.0.0.3*.zip" \
+  --dat-name-regex "/arcade/i" \
+  --input  "${input_dir}/FBNeo-1.0.0.3" \
+  --output "${output_dir}/fbneo"
+
+echo "Copying MAME roms..."
+igir copy zip clean test \
+  --dat "./dat/MAME 0.279.zip" \
+  --input  "${input_dir}/MAME" \
+  --output "${output_dir}/mame" \
+  --temp-dir "./.igir_tmp"
+
+# Rename CHD files that lack extensions
+find "${output_dir}/mame" -mindepth 2 -type f ! -name "*.*" \
+  -exec mv "{}" "{}.chd" \;
+
