c40234fe0b9a4126bac135f694099e82c44ebd75
Chris <chris@wookie.local>
2025-09-01 18:49:31 -0700

    Narrow target dir rename of CHD in MAME.

--- process-mame.sh
+++ process-mame.sh
@@ -14,7 +14,7 @@ npx --yes igir@latest copy zip clean test \
   --merge-roms split
 
 # rename extensionless files to .chd
-find ${output_dir} -mindepth 2 -type f ! -name "*.*" -exec mv "{}" "{}.chd" \;
+find "${output_dir}/MAME" -mindepth 2 -type f ! -name "*.*" -exec mv "{}" "{}.chd" \;
 
 # # MAME-2003-Plus
 npx --yes igir@latest copy zip clean test \
@@ -24,3 +24,5 @@ npx --yes igir@latest copy zip clean test \
   --input-checksum-quick \
   --input-checksum-archives never \
   --merge-roms split
+
+find "${output_dir}/MAME-2003-Plus" -mindepth 2 -type f ! -name "*.*" -exec mv "{}" "{}.chd" \;
