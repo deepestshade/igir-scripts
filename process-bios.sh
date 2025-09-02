055ee721cee367783a96c4ef2c6b9e1425804898
Chris <chris@wookie.local>
2025-09-01 18:36:21 -0700

    Remove extract from BIOS prep

--- process-bios.sh
+++ process-bios.sh
@@ -3,7 +3,7 @@
 input_dir="./input/BIOS"
 output_dir="./intermediate/BIOS"
 
-npx --yes igir@latest copy extract clean test \
+npx --yes igir@latest copy clean test \
   --dat "https://raw.githubusercontent.com/libretro/libretro-database/master/dat/System.dat" \
   --input "${input_dir}/" \
   --output "${output_dir}" 
